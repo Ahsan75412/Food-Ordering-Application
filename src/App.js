85cbf8339b04426aef5e7bf42cf2ea984d69a7d9
Ahsan Habib <ahsanpub16b@gmail.com>
2023-01-15 16:32:16 +0600

    Project initialli done here!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
-import Footer from './Pages/Footer';
+import Footer from './Components/Footer';
 import Home from './Pages/Home';
+import Menu from './Pages/Menu';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/menu' element={<Menu></Menu>} ></Route>
+        <Route path='/about' element={<About></About>} ></Route>
+        <Route path='/contact' element={<Contact></Contact>} ></Route>
       </Routes>
       <Footer></Footer>
 
